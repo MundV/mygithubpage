65b6b90ed9d6984d2fa120daa73699a1357fd128
Mund Vetter <mundvetter@gmail.com>
2017-07-28 16:21:21 +0200

    complete service worker

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
-/* globals toolbox importScripts addEventListener skipWaiting clients */
+/* globals toolbox importScripts addEventListener skipWaiting clients serviceWorkerOption*/
 importScripts('https://unpkg.com/sw-toolbox@3.6.0/sw-toolbox.js')
 addEventListener('install', event => event.waitUntil(skipWaiting()))
 addEventListener('activate', event => event.waitUntil(clients.claim()))
 toolbox.options.debug = true
-toolbox.precache(['/', '/static/options/default.json', '/static/options/twin.json', '/sw.js', '/app.js'])
+toolbox.precache(serviceWorkerOption.assets)
 toolbox.router.get('/*', toolbox.fastest)
